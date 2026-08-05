0fb4388a789ab8d3c02bb9e0f363557588f0c333
waheedoof <Adamjarvi0@gmail.com>
2026-08-05 17:02:29 -0600

    first commit 2

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import { Client, Databases, Query } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1')
-    .setProject(process.env.APPWRITE_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key']);
 
   const databases = new Databases(client);
 
